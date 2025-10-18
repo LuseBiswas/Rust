1dbbf2f56fa9f6c998c14ac854c07038ab81bf32
LuseBiswas <lusebiswas@gmail.com>
2025-10-18 14:23:20 +0530

    Rust Notes

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+fn main() {
+    // let x: i32 = 5;
+
+    let greeting: String = String::from("Hello world!");
+    println!("{}", greeting);
+    // println!("Iteration is Happening: {}", get_first_word(greeting));
+
+    // let im_male: bool = true;
+    // println!("{}", im_male);
+
+    // for i in 0..10 {
+    //     x = x + i;
+    //     println!("{}", x);
+    // }
+
+}
+
+// fn get_first_word(greeting: String)-> String {
+//     let mut ans: String = String::from("");
+//     for char in greeting.chars(){
+//         if char == ' '{
+//             break;
+//         }
+//         ans.push(char);
+//     }
+//     return  ans;
+// }
+
